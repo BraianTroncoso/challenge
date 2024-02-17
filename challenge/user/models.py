d4e85c996fb6c18bcc1306444468003754d34382
Braian <troncosobraianaxel@gmail.com>
2024-02-17 00:28:51 -0300

    [ADD] model user

--- challenge/user/models.py
+++ challenge/user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class UserModel(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    phone = models.BigIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "person"
+        ordering = ['-created_at']
