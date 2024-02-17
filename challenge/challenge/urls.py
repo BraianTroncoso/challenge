a57a744396258afb7122aa7f93f6710b302491e2
Braian <troncosobraianaxel@gmail.com>
2024-02-17 11:30:35 -0300

    [FIX] refactoring serializer, routes, core

--- challenge/challenge/urls.py
+++ challenge/challenge/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from user import views
 
-
 urlpatterns = [
-    path('users/',views.userApi),
-    path('users/<int:id>',views.userApi),
+    path('user/', views.userApi),
+    path('user/<int:id>/', views.userApi),
     path('admin/', admin.site.urls),
 ]
--- challenge/user/serializers.py
+++ challenge/user/serializers.py
@@ -5,5 +5,5 @@ from user.models import UserModel
 class UserSerializer(ModelSerializer):
     class Meta:
         model = UserModel
-        fields = ['id', 'name', 'address', 'fee']
-        #fields = '__all__'
+        # fields = ['id', 'name', 'address', 'fee']
+        fields = '__all__'
