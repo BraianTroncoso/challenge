4a54d0ebe5a408d06a1c7d0e86b92882b072ddaa
Braian <troncosobraianaxel@gmail.com>
2024-02-18 19:45:50 -0300

    [ADD] validation model backend

--- challenge/user_entity/models.py
+++ challenge/user_entity/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-
-# Create your models here.
+from django.core.validators import MinLengthValidator, MaxLengthValidator, MinValueValidator, MaxValueValidator
 
 class UserEntity(models.Model):
-    first_name = models.CharField(max_length = 255)
-    last_name = models.CharField(max_length = 255)
-    address = models.CharField(max_length = 255)
-    phone = models.IntegerField()
+    first_name = models.CharField(max_length=255, validators=[MinLengthValidator(2), MaxLengthValidator(255)])
+    last_name = models.CharField(max_length=255, validators=[MinLengthValidator(2), MaxLengthValidator(255)])
+    address = models.CharField(max_length=255, validators=[MinLengthValidator(5), MaxLengthValidator(255)])
+    phone = models.CharField(max_length=15, validators=[MinLengthValidator(10), MaxLengthValidator(15)])
